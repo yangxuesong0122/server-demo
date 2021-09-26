da02c9f2c30aa885b9b1f882282d749826d1597e
yxs <y_xs@winning.com.cn>
2021-09-26 11:58:25 +0800

    node开发练习

---
+++ serve.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+app.use(express.static(__dirname + '/static'))
+app.get('/person', (req, res) => {
+    res.send({
+        name: 'yxs',
+        age: 27
+    })
+})
+app.listen(5005, (err) => {
+    if (!err) {
+        console.log('服务器启动成功了')
+    }
+})
